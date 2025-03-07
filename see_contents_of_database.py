6c9cba7a80d60a118d7ed82ee8ddc77836bb78eb
Dave Austin <49787780+austindi@users.noreply.github.com>
2025-03-06 19:55:06 -0500

    Update see_contents_of_database.py

--- see_contents_of_database.py
+++ see_contents_of_database.py
@@ -1,10 +1,11 @@
 import duckdb
 
-con = duckdb.connect( "database/bitcoin.db" ) 
+conn = duckdb.connect("database/bitcoin.db")
 
-print( "Available tables:", conn.execute("SHOW TABLES").fetchall())
+print("Available tables:", conn.execute("SHOW TABLES").fetchall())
 
-df - con.execute( "SELECT * FROM crypto_data" ).df() 
+df = conn.execute("SELECT * FROM crypto_data").df()
 
-print( df ) 
-con.close() 
+print(df)
+
+conn.close()
